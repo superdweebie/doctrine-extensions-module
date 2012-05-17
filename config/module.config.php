a173d61e28b8e396e2a94d346827254fae4dbc9a
Tim Roediger <tim.roediger@ausaero.com.au>
2012-05-17 16:03:17 +1000

    Removed unused and clunky code - the find classes

--- config/module.config.php
+++ config/module.config.php
@@ -7,6 +7,7 @@ return array(
                 'injections' => array(
                     'setSubscriber' => array(
                         array('subscriber' => 'SdsDoctrineExtensions\ActiveUser\Listener\ActiveUser'),
+                        array('subscriber' => 'SdsDoctrineExtensions\AccessControl\Listener\AccessControl'),                   
                         array('subscriber' => 'SdsDoctrineExtensions\Audit\Listener\Audit'),
                         array('subscriber' => 'SdsDoctrineExtensions\Readonly\Listener\Readonly'),                    
                         array('subscriber' => 'SdsDoctrineExtensions\SoftDelete\Listener\SoftDelete'),                          
@@ -21,6 +22,12 @@ return array(
               ),  
             ),
 
+            'SdsDoctrineExtensions\AccessControl\Listener\AccessControl' => array(
+              'parameters' => array(
+                  'activeUser' => 'active_user',
+              ),  
+            ),
+            
             'SdsDoctrineExtensions\Audit\Listener\Audit' => array(
               'parameters' => array(
                   'activeUser' => 'active_user',
@@ -61,59 +68,5 @@ return array(
                 ),
             ), 
         ),        
-        'definition' => array(
-            'class' => array(
-                'SdsDoctrineExtensionsModule\Factory\Find' => array(
-                    'instantiator' => array(
-                        'SdsDoctrineExtensionsModule\Factory\Find', 'get',                      
-                    ),
-                    'methods' => array(
-                        'get' => array(
-                            'objectManager' => array('type' => 'Doctrine\Common\Persistence\ObjectManager', 'required' => true),
-                            'objectClassName' => array('type' => false, 'required' => true),
-                            'id' => array('type' => false, 'required' => true),                            
-                        ),                                             
-                    ),
-                ),  
-                'SdsDoctrineExtensionsModule\Factory\FindAll' => array(
-                    'instantiator' => array(
-                        'SdsDoctrineExtensionsModule\Factory\FindAll', 'get'                        
-                    ),
-                    'methods' => array(
-                        'get' => array(
-                            'objectManager' => array('type' => 'Doctrine\Common\Persistence\ObjectManager', 'required' => true),
-                            'objectClassName' => array('type' => false, 'required' => true),                         
-                        ),                                                
-                    ),
-                ),                 
-                'SdsDoctrineExtensionsModule\Factory\FindBy' => array(
-                    'instantiator' => array(
-                        'SdsDoctrineExtensionsModule\Factory\FindBy', 'get'                        
-                    ),
-                    'methods' => array(
-                        'get' => array(
-                            'objectManager' => array('type' => 'Doctrine\Common\Persistence\ObjectManager', 'required' => true),
-                            'objectClassName' => array('type' => false, 'required' => true),
-                            'criteria' => array('type' => false, 'required' => false),  
-                            'orderBy' => array('type' => false, 'required' => false),    
-                            'limit' => array('type' => false, 'required' => false),
-                            'offset' => array('type' => false, 'required' => false),                                                   
-                        ),                                                
-                    ),
-                ), 
-                'SdsDoctrineExtensionsModule\Factory\FindOneBy' => array(
-                    'instantiator' => array(
-                        'SdsDoctrineExtensionsModule\Factory\FindOneBy', 'get'                        
-                    ),
-                    'methods' => array(
-                        'get' => array(
-                            'objectManager' => array('type' => 'Doctrine\Common\Persistence\ObjectManager', 'required' => true),
-                            'objectClassName' => array('type' => false, 'required' => true),
-                            'criteria' => array('type' => false, 'required' => false),                                                 
-                        ),                                                
-                    ),
-                ), 
-            ),
-        ),
     ),    
 );
