01ad6d0bab0c3658f7ed8ee7f7db181cc13121bd
Tim Roediger <tim.roediger@ausaero.com.au>
2012-06-29 13:27:09 +1000

    WIP syncing with changes to SdsDoctrineExtensions

--- config/module.config.php
+++ config/module.config.php
@@ -1,54 +1,21 @@
 <?php
 return array(
-    'sdsDoctrineExtensions' => array(        
+    'sdsDoctrineExtensions' => array(
         'activeUser' => 'SdsAuthModule\ActiveUser',
-        'annotationReader' => 'Doctrine\Common\Annotations\CachedReader'
+        'annotationReader' => 'Doctrine\Common\Annotations\CachedReader',
+        'extensions' => array(
+            'SdsDoctrineExtensions\AccessControl' => null,
+            'SdsDoctrineExtensions\Audit' => null,
+            'SdsDoctrineExtensions\DoNotHardDelete' => null,
+            'SdsDoctrineExtensions\Freeze' => null,
+            'SdsDoctrineExtensions\Readonly' => null,
+            'SdsDoctrineExtensions\Serializer' => null,
+            'SdsDoctrineExtensions\SoftDelete' => null,
+            'SdsDoctrineExtensions\Stamp' => null,
+            'SdsDoctrineExtensions\State' => null,
+            'SdsDoctrineExtensions\UiHints' => null,
+            'SdsDoctrineExtensions\Workflow' => null,
+            'SdsDoctrineExtensions\Zone' => null,
+        ),
     ),
-    'doctrine' => array(
-        'drivers' => array(
-            'odm' => array(
-                array(
-                    'class' => 'Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver',
-                    'namespace' => 'SdsDoctrineExtensions\Audit\Model',
-                    'paths' => array(
-                        'vendor/superdweebie/SdsDoctrineExtensions/lib/SdsDoctrineExtensions/Audit/Model'
-                    ),                            
-                ),  
-                array(
-                    'class' => 'Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver',
-                    'namespace' => 'SdsDoctrineExtensions\AccessControl\Model',
-                    'paths' => array(
-                        'vendor/superdweebie/SdsDoctrineExtensions/lib/SdsDoctrineExtensions/AccessControl/Model'
-                    ),                         
-                ),                 
-            )
-        ),
-        'filters' => array(
-            'odm' => array(
-                'readAccessControl' => 'SdsDoctrineExtensions\AccessControl\Filter\ReadAccessControl',                
-            )
-        ),
-        'subscribers' => array(
-            'odm' => array(
-                'SdsDoctrineExtensions\ActiveUser\Listener\ActiveUser',
-                'SdsDoctrineExtensions\AccessControl\Listener\AccessControl',                   
-                'SdsDoctrineExtensions\Audit\Listener\Audit',
-                'SdsDoctrineExtensions\Readonly\Listener\Readonly',                    
-                'SdsDoctrineExtensions\SoftDelete\Listener\SoftDelete',                          
-                'SdsDoctrineExtensions\Serializer\Listener\Serializer', 
-                'SdsDoctrineExtensions\Stamp\Listener\Stamp',             
-                'SdsDoctrineExtensions\Metadata\Listener\Metadata',                  
-            )
-        ),
-        'annotations' => array(
-            'odm' => array(
-                'vendor\superdweebie\SdsDoctrineExtensions\lib\SdsDoctrineExtensions\Audit\Mapping\Annotation\Audit.php',
-                'vendor\superdweebie\SdsDoctrineExtensions\lib\SdsDoctrineExtensions\Readonly\Mapping\Annotation\Readonly.php',
-                'vendor\superdweebie\SdsDoctrineExtensions\lib\SdsDoctrineExtensions\Serializer\Mapping\Annotation\DoNotserialize.php',
-                'vendor\superdweebie\SdsDoctrineExtensions\lib\SdsDoctrineExtensions\Metadata\Mapping\Annotation\Label.php',     
-                'vendor\superdweebie\SdsDoctrineExtensions\lib\SdsDoctrineExtensions\Metadata\Mapping\Annotation\Width.php',  
-                'vendor\superdweebie\SdsDoctrineExtensions\lib\SdsDoctrineExtensions\Metadata\Mapping\Annotation\Hidden.php',      
-            )
-        )    
-    ),    
 );
--- src/SdsDoctrineExtensionsModule/Module.php
+++ src/SdsDoctrineExtensionsModule/Module.php
@@ -2,74 +2,69 @@
 
 namespace SdsDoctrineExtensionsModule;
 
+use DoctrineMongoODModule\Events;
+use SdsDoctrineExtensions\Manifest;
+use SdsDoctrineExtensions\ManifestConfig;
 use Zend\ModuleManager\ModuleManager;
 use Zend\EventManager\Event;
-use SdsDoctrineExtensions\Serializer\SerializerService;
-use SdsInitalizerModule\Service\Events as InitalizerEvents;
 
 class Module
 {
-    
+
+    protected $manifest;
+
     public function init(ModuleManager $moduleManager){
         $sharedEvents = $moduleManager->events()->getSharedManager();
         $sharedEvents->attach(
-            InitalizerEvents::IDENTIFIER, 
-            InitalizerEvents::LOAD_CONTROLLER_LOADER_INITALIZERS, 
-            array($this, 'loadInitalizers')
+            Events::identifier,
+            Events::getSubscribers,
+            array($this, 'getSubscribers')
         );
         $sharedEvents->attach(
-            InitalizerEvents::IDENTIFIER, 
-            InitalizerEvents::LOAD_SERVICE_MANAGER_INITALIZERS, 
-            array($this, 'loadInitalizers')
-        );        
-    }
-    
-    public function loadInitalizers(Event $event){
-        $serviceLocator = $event->getTarget();
-        $config = $serviceLocator->get('Configuration');
-        $config = $config['sdsDoctrineExtensions'];
-        return array(
-            'AnnotationReaderAwareInterface' =>
-                function ($instance) use ($serviceLocator, $config) {
-                    if ($instance instanceof AnnotationReaderAwareInterface) {
-                        $instance->setAnnotationReader($serviceLocator->get($config['annotationReader']));
-                    }
-                },
-            'ActiveUserAwareInterface' =>
-                function ($instance) use ($serviceLocator, $config) {
-                    if ($instance instanceof ActiveUserAwareInterface) {
-                        $instance->setActiveUser($serviceLocator->get($config['activeUser']));
-                    }
-                }                
+            Events::identifier,
+            Events::getAnnotations,
+            array($this, 'getAnnotations')
         );
-    }    
-    
+        $sharedEvents->attach(
+            Events::identifier,
+            Events::getFilters,
+            array($this, 'getFilters')
+        );
+    }
+
+    public function getManifest(ServiceLocator $serviceLocator){
+        if (!isset($this->manifest)) {
+            $config = $serviceLocator->get('Configuration')['sdsDoctrineExtensions'];
+            $activeUser = $serviceLocator->get($config['activeUser']);
+            $annotationReader = $serviceLocator->get($config['annotationReader']);
+
+            $manifestConfig = new ManifestConfig($annotationReader, null, $activeUser);
+
+            foreach ($config['extensions'] as $namespace => $extensionConfigArray) {
+                $extensionConfigClass = $namespace . '\ExtensionConfig';
+                $extensionConfig = new $extensionConfigClass($extensionConfigArray);
+                $manifestConfig->addExtensionConfig($namespace, $extensionConfig);
+            }
+
+            $this->manifest = new Manifest($manifestConfig);
+        }
+        return $this->manifest;
+    }
+
+    public function getSubscribers(Event $event){
+        return $this->getManifest($event->getTarget())->getSubscribers();
+    }
+
+    public function getAnnotations(Event $event){
+        return $this->getManifest($event->getTarget())->getAnnotations();
+    }
+
+    public function getFilters(Event $event){
+        return $this->getManifest($event->getTarget())->getFilters();
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/../../config/module.config.php';
     }
-
-    public function onBootstrap(Event $e){       
-        $app = $e->getParam('application');        
-        $serviceLocator = $app->getServiceManager();
-        $documentManager = $serviceLocator->get('Doctrine\ODM\MongoDB\DocumentManager');
-        
-        $serializerService = SerializerService::getInstance();
-        $serializerService->setDocumentManager($documentManager);                  
-        
-        $config = $serviceLocator->get('Configuration');
-        $config = $config['sdsDoctrineExtensions'];        
-        $activeUser = $serviceLocator->get($config['activeUser']);
-        $filter = $documentManager->getFilterCollection()->enable('readAccessControl');        
-        $filter->setParameter('activeUser', $activeUser);
-    }        
-       
-//    public function getServiceConfiguration()
-//    {
-//        return array(
-//            'abstract_factories' => array(
-//                'SdsDoctrineExtensions\Common\Listener\AbstractListener' => 'SdsDoctrineExtensionsModule\Service\ListenerFactory',
-//            )
-//        );
-//    }  
 }
