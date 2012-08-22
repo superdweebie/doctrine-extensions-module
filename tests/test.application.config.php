1add304efde2ebdf3423a8bcca0984a674c287fa
Tim Roediger <tim.roediger@ausaero.com.au>
2012-08-22 16:37:33 +1000

    Third time lucky

--- tests/test.application.config.php
+++ tests/test.application.config.php
@@ -7,7 +7,7 @@ return array(
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
-            __DIR__ . '/test.module.config.php',
+            'vendor/superdweebie/doctrine-extensions-module/tests/test.module.config.php',
         ),
         'module_paths' => array(
             './vendor',
